31d0e37e479e981b7e64be35bfb495d5c2fdf5fc
Alvaro-Calleros <alvarocalleros44@gmail.com>
2024-12-16 09:23:52 -0700

    Added Black & White Diamond heart from Jewerly

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -20,6 +20,10 @@ test('test-PURCHASE', async ({ page }) => {
   await page.getByLabel('Password:').click();
   await page.getByLabel('Password:').fill('SecurePass123');
   await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Jewelry' }).first().click();
+  await page.getByRole('link', { name: 'Black & White Diamond Heart', exact: true }).click();
+  await page.getByRole('button', { name: 'Add to cart' }).click();
+  await page.getByTitle('Close').click();
   await page.getByRole('link', { name: 'Electronics' }).first().click();
   await page.getByRole('heading', { name: 'Cell phones' }).getByRole('link').click();
   await page.getByRole('link', { name: 'Phone Cover', exact: true }).click();
@@ -33,12 +37,15 @@ test('test-PURCHASE', async ({ page }) => {
     await page.locator('#add-to-cart-button-80').click();
     expect(await page.waitForSelector('text=The product has been added to your shopping cart', { state: 'visible' })).toBeTruthy();
   }
+
   await page.getByRole('link', { name: 'Shopping cart' }).first().click();
   await page.locator('#termsofservice').check();
   await page.locator('.checkout-buttons').click();
   for (let i = 0; i < 5; i++) { //aqui hice un ciclo porque eran 5 "Continue" seguidos
     await page.locator('//input[@value="Continue"]').nth(i).click();
   }
+
+
   await page.getByRole('button', { name: 'Confirm' }).click();
   const successPurchase = await page.locator('text=Your order has been successfully processed!');
   await expect(successPurchase).toBeVisible();
